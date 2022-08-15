a57cd140b99d92d1391eee4f884fb05982ea5ca9
yshalsager <ysh-alsager@hotmail.com>
2022-08-15 13:29:58 +0200

    google forms: Update with new website changes
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- anki_scripts/extractors/google_form.py
+++ anki_scripts/extractors/google_form.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from bs4 import BeautifulSoup
 from bs4.element import Tag, ResultSet
 from requests import get
@@ -14,15 +12,15 @@ class GoogleFormExtractor(BaseExtractor):
         self.url = url
         self._resp = get(url)
         self.html = BeautifulSoup(self._resp.text, "html.parser")
-        self._quiz_selector = '.freebirdFormviewerComponentsQuestionBaseRoot, .freebirdFormviewerViewItemsItemItem'
-        self._question_selector = ".freebirdFormviewerComponentsQuestionBaseTitle, " \
-                                  ".freebirdFormviewerViewItemsItemItemTitle"
-        self._answer_selector = "span.docssharedWizToggleLabeledLabelText"
-        self._correct_answer_selector = f".freebirdFormviewerViewItemsRadioCorrect {self._answer_selector}, " \
-                                        f".freebirdFormviewerViewItemsItemGradingCorrectAnswerBox " \
-                                        f"{self._answer_selector}, .freebirdFormviewerViewItemsCheckboxCorrect " \
-                                        f"{self._answer_selector}"
-        self._feedback_selector = f".freebirdFormviewerViewItemsItemGradingFeedbackText"
+        self._quiz_selector = 'div[role="listitem"]'
+        self._question_selector = 'div[role="heading"] span'
+        self._answers_selector = 'span[role="presentation"]'
+        self._answer_selector = "label[for]"
+        self._answer_text_selector = "span"
+        self._correct_answer_selector = 'div[aria-level="3"]'
+        self._answer_is_correct_selector = 'div[aria-label="إجابة صحيحة"], div[aria-label="Correct"]'
+        self._correct_answer_text_list = ["Correct answer", "الإجابة الصحيحة"]
+        self._feedback_selector = 'div[role="group"]'
 
     def extract_quiz_questions(self):
         quiz_questions_list = []
@@ -32,25 +30,38 @@ class GoogleFormExtractor(BaseExtractor):
             question = quiz_question.select_one(self._question_selector)
             if not question:
                 continue
-            if question.span:
-                # Remove the asterisk (freebirdFormviewerComponentsQuestionBaseRequiredAsterisk)
-                question.span.decompose()
+            if not quiz_question.select_one(self._answers_selector):
+                continue
             answers_html: ResultSet = quiz_question.select(self._answer_selector)
             answers = []
+            correct_answers = []
+            got_correct_answers = False
+            answer: Tag
             for answer in answers_html:
-                answer_text = answer.text.strip()
+                answer_text = answer.select_one(self._answer_text_selector).text.strip()
                 if answer_text in answers:
                     # Skip duplicate answer (in correct answer box)
                     continue
                 answers.append(answer_text)
-            chosen_answers = quiz_question.select(self._correct_answer_selector)
-            if chosen_answers:
-                chosen_answers = [i.text.strip() for i in chosen_answers]
+                if answer.select(self._answer_is_correct_selector):
+                    correct_answers.append(answer_text)
+                    got_correct_answers = True
+            if not got_correct_answers:
+                answers_divs = quiz_question.select(self._correct_answer_selector)
+                if answers_divs:
+                    correct_answers_div = list(
+                        filter(lambda x: any(i for i in self._correct_answer_text_list if i == x.text), answers_divs))
+                    if correct_answers_div:
+                        for correct_answer in quiz_question.select(
+                                f'.{" ".join(correct_answers_div[-1].get("class"))} ~ div {self._answer_selector}'):
+                            correct_answer_text = correct_answer.select_one(self._answer_text_selector).text.strip()
+                            if correct_answer_text not in correct_answers:
+                                correct_answers.append(correct_answer_text)
             feedback_text = quiz_question.select_one(self._feedback_selector)
             if feedback_text:
                 feedback_text = feedback_text.text.strip()
             quiz_questions_list.append(
-                Question(question.text.strip(), answers, correct_answers=chosen_answers, feedback=feedback_text))
+                Question(question.text.strip(), answers, correct_answers, feedback=feedback_text))
         return Quiz(quiz_questions_list)
 
     def __repr__(self):
